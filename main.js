07aa6203a352d4a0a2b7645c9076ffc6aa0655e3
Elias <elias.persson4@elev.ga.ntig.se>
2024-09-09 14:59:31 +0200

    rename pictures and allat

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const bgElement = document.querySelector(".bg")
 function Relocate(location){
-    bgElement.style.backgroundImage = "url(img/"+location+".jpg)"
+    bgElement.style.backgroundImage = "url(img/"+location+".png)"
 }
 
-Relocate("deep_forest")
+Relocate("volcano")
