be95b687296ad2f09281d89edf036d6bb8713080
Elias <elias.persson4@elev.ga.ntig.se>
2024-09-10 11:36:12 +0200

    Added some text

--- main.js
+++ main.js
@@ -1,24 +1,41 @@
 const bgElement = document.querySelector(".bg")
+
+const healthElement = document.querySelector(".health")
+let health = 90
+const foodElement = document.querySelector(".food")
+let food = 100
+const waterElement = document.querySelector(".water")
+let water = 60
+
 const travelElement = document.querySelector(".travel-menu")
 const locationElement = document.querySelector(".location")
 const locations = ["beach", "deep_forest", "forest", "hotspring", "lake", "river", "ruins", "shipwreck", "volcano"]
-
+let currentLocation = "beach"
 function Relocate(location){
     bgElement.style.backgroundImage = "url(img/"+location+".png)"
 }
+
+function UpdateStats(){
+    healthElement.style.width = health+"%"
+    foodElement.style.width = food+"%"
+    waterElement.style.width = water+"%"
+}
+
+UpdateStats()
 
 locations.forEach(element => {
     let listElement = document.createElement("li")
     listElement.className = "dropdown-item"
 
     let buttonElement = document.createElement("button")
-    buttonElement.textContent = element
+    buttonElement.textContent = element.replace("_", " ")
     buttonElement.className = "btn btn-secondary travel-btn w-100"
     buttonElement.addEventListener("click", function(){
-        Relocate(buttonElement.textContent)
-        locationElement.innerHTML = buttonElement.textContent.replace("_", " ")
+        Relocate(buttonElement.textContent.replace(" ", "_"))
+        locationElement.innerHTML = buttonElement.textContent
+        currentLocation = buttonElement.textContent
     })
-    
+
     listElement.appendChild(buttonElement)
     travelElement.appendChild(listElement)
 });
