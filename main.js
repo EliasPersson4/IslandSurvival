ee64480f7c7a1e63e2ac2ccae5b9f06085442d0f
Elias <elias.persson4@elev.ga.ntig.se>
2024-09-10 09:02:14 +0200

    updated ui

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const bgElement = document.querySelector(".bg")
+const travelElement = document.querySelector(".travel")
+
 function Relocate(location){
     bgElement.style.backgroundImage = "url(img/"+location+".png)"
 }
-
+const locations = ["beach", "deep_forest", "forest", "hotspring", "lake", "river", "ruins", "shipwreck", "volcano"]
 Relocate("deep_forest")
