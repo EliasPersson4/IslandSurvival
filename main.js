f56c99acd79a41d82a0194a2ba77e099a5329fbb
Elias <elias.persson4@elev.ga.ntig.se>
2024-09-12 12:56:51 +0200

    typescript implementation

--- main.js
+++ main.js
@@ -1,82 +1,80 @@
-const bgElement = document.querySelector(".bg")
-
-const healthElement = document.querySelector(".health")
-let health = 90
-const foodElement = document.querySelector(".food")
-let food = 100
-const waterElement = document.querySelector(".water")
-let water = 60
-
-const travelElement = document.querySelector(".travel-menu")
-const locationElement = document.querySelector(".location")
-const audioElement = document.querySelector(".music")
-const interactElement = document.querySelector(".interact-menu")
-const huntGatherElement = document.querySelector(".hunt-gather-menu")
-
-const locations = ["beach", "deep_forest", "forest", "hotspring", "lake", "river", "ruins", "shipwreck", "volcano"]
-const interactions = ["sleep", "investigate", "stroke_yo_bone"]
-const huntGather = ["Hunt", "Fish", "Gather"]
-
-let currentLocation = "beach"
-
-function Relocate(location){
-    bgElement.style.backgroundImage = "url(img/"+location+".png)"
-
-    audioElement.setAttribute("src", "mp3/"+location+".mp3")
-
-    locationElement.innerHTML = location
-
-    currentLocation = location
+var _a;
+var bgElement = document.querySelector(".bg");
+var healthElement = document.querySelector(".health");
+var health = 90;
+var foodElement = document.querySelector(".food");
+var food = 100;
+var waterElement = document.querySelector(".water");
+var water = 60;
+var travelElement = document.querySelector(".travel-menu");
+var locationElement = document.querySelector(".location");
+var audioElement = document.querySelector(".music");
+var interactElement = document.querySelector(".interact-menu");
+var huntGatherElement = document.querySelector(".hunt-gather-menu");
+var locations = ["beach", "deep_forest", "forest", "hotspring", "lake", "river", "ruins", "shipwreck", "volcano"];
+var interactions = ["sleep", "investigate", "stroke_yo_bone"];
+var huntGather = ["Hunt", "Fish", "Gather"];
+var currentLocation = "beach";
+function Relocate(location) {
+    if (bgElement) {
+        bgElement.style.backgroundImage = "url(img/".concat(location, ".png)");
+    }
+    if (audioElement) {
+        audioElement.setAttribute("src", "mp3/music/".concat(location, ".mp3"));
+    }
+    if (locationElement) {
+        locationElement.textContent = location;
+    }
+    currentLocation = location;
 }
-
-function UpdateStats(){
-    healthElement.style.width = health+"%"
-    foodElement.style.width = food+"%"
-    waterElement.style.width = water+"%"
+function UpdateStats() {
+    if (healthElement) {
+        healthElement.style.width = "".concat(health, "%");
+    }
+    if (foodElement) {
+        foodElement.style.width = "".concat(food, "%");
+    }
+    if (waterElement) {
+        waterElement.style.width = "".concat(water, "%");
+    }
 }
-
-UpdateStats()
-
-function PopulateDropdown(parent, array, ...extraCss){
-    array.forEach(element => {
-        let listElement = document.createElement("li")
-        listElement.className = "dropdown-item"
-        
-        let buttonElement = document.createElement("button")
-        buttonElement.textContent = element.replace("_", " ")
-        buttonElement.className = "btn btn-secondary w-100 "+extraCss
-        
-        listElement.appendChild(buttonElement)
-        parent.appendChild(listElement)
+function PopulateDropdown(parent, array) {
+    var extraCss = [];
+    for (var _i = 2; _i < arguments.length; _i++) {
+        extraCss[_i - 2] = arguments[_i];
+    }
+    if (parent) {
+        array.forEach(function (element) {
+            var listElement = document.createElement("li");
+            listElement.className = "dropdown-item";
+            var buttonElement = document.createElement("button");
+            buttonElement.textContent = element.replace("_", " ");
+            buttonElement.className = "btn btn-secondary w-100 ".concat(extraCss.join(" "));
+            listElement.appendChild(buttonElement);
+            parent.appendChild(listElement);
+        });
+    }
+}
+UpdateStats();
+PopulateDropdown(interactElement, interactions, "interact-btn");
+PopulateDropdown(travelElement, locations, "travel-btn");
+PopulateDropdown(huntGatherElement, huntGather, "hunt-gather-btn");
+document.querySelectorAll(".travel-btn").forEach(function (element) {
+    element.addEventListener("click", function () {
+        Relocate(element.textContent.trim());
     });
-}
-
-
-PopulateDropdown(interactElement, interactions, "interact-btn")
-PopulateDropdown(travelElement, locations, "travel-btn")
-PopulateDropdown(huntGatherElement, huntGather, "hunt-gather-btn")
-
-document.querySelectorAll(".travel-btn").forEach(element => {
-    element.addEventListener("click", function(){
-            
-        Relocate(element.innerHTML)
-        
-    })
 });
-
-let isPlaying = true;
-
+var isPlaying = !(audioElement === null || audioElement === void 0 ? void 0 : audioElement.muted);
 function togglePlay() {
-    isPlaying ? audioElement.muted = true : audioElement.muted = false
-    isPlaying = !isPlaying
-};
-
-document.querySelectorAll("button").forEach(element => {
-    element.addEventListener("click", function(){
-        var audio = new Audio('mp3/sfx/button'+(1+Math.floor(Math.random()*3))+'.mp3');
-        audio.play()
-
-    })
+    if (audioElement) {
+        audioElement.muted = isPlaying;
+    }
+    isPlaying = !isPlaying;
+}
+document.querySelectorAll("button").forEach(function (element) {
+    element.addEventListener("click", function () {
+        var audio = new Audio("mp3/sfx/button".concat(1 + Math.floor(Math.random() * 3), ".mp3"));
+        audio.play();
+    });
 });
-
-document.querySelector(".music-toggle").addEventListener("click", togglePlay)
+(_a = document.querySelector(".music-toggle")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", togglePlay);
---
+++ main.ts
@@ -0,0 +1,93 @@
+const bgElement: HTMLElement | null = document.querySelector(".bg");
+
+const healthElement: HTMLElement | null = document.querySelector(".health");
+let health: number = 90;
+const foodElement: HTMLElement | null = document.querySelector(".food");
+let food: number = 100;
+const waterElement: HTMLElement | null = document.querySelector(".water");
+let water: number = 60;
+
+const travelElement: HTMLElement | null = document.querySelector(".travel-menu");
+const locationElement: HTMLElement | null = document.querySelector(".location");
+const audioElement: HTMLAudioElement | null = document.querySelector(".music");
+const interactElement: HTMLElement | null = document.querySelector(".interact-menu");
+const huntGatherElement: HTMLElement | null = document.querySelector(".hunt-gather-menu");
+
+const locations: string[] = ["beach", "deep_forest", "forest", "hotspring", "lake", "river", "ruins", "shipwreck", "volcano"];
+const interactions: string[] = ["sleep", "investigate", "stroke_yo_bone"];
+const huntGather: string[] = ["Hunt", "Fish", "Gather"];
+
+let currentLocation: string = "beach";
+
+function Relocate(location: string): void {
+    if (bgElement) {
+        bgElement.style.backgroundImage = `url(img/${location}.png)`;
+    }
+
+    if (audioElement) {
+        audioElement.setAttribute("src", `mp3/music/${location}.mp3`);
+    }
+    if (locationElement) {
+        locationElement.textContent = location;
+    }
+
+    currentLocation = location;
+}
+
+function UpdateStats(): void {
+    if (healthElement) {
+        healthElement.style.width = `${health}%`;
+    }
+    if (foodElement) {
+        foodElement.style.width = `${food}%`;
+    }
+    if (waterElement) {
+        waterElement.style.width = `${water}%`;
+    }
+}
+
+
+function PopulateDropdown(parent: HTMLElement | null, array: string[], ...extraCss: string[]): void {
+    if (parent) {
+        array.forEach(element => {
+            let listElement: HTMLElement = document.createElement("li");
+            listElement.className = "dropdown-item";
+            
+            let buttonElement: HTMLButtonElement = document.createElement("button");
+            buttonElement.textContent = element.replace("_", " ");
+            buttonElement.className = `btn btn-secondary w-100 ${extraCss.join(" ")}`;
+            
+            listElement.appendChild(buttonElement);
+            parent.appendChild(listElement);
+        });
+    }
+}
+
+UpdateStats();
+PopulateDropdown(interactElement, interactions, "interact-btn");
+PopulateDropdown(travelElement, locations, "travel-btn");
+PopulateDropdown(huntGatherElement, huntGather, "hunt-gather-btn");
+
+document.querySelectorAll(".travel-btn").forEach(element => {
+    element.addEventListener("click", function () {
+        Relocate(element.textContent!.trim());
+    });
+});
+
+let isPlaying: boolean = !audioElement?.muted;
+
+function togglePlay(): void {
+    if (audioElement) {
+        audioElement.muted = isPlaying;
+    }
+    isPlaying = !isPlaying;
+}
+
+document.querySelectorAll("button").forEach(element => {
+    element.addEventListener("click", function () {
+        const audio: HTMLAudioElement = new Audio(`mp3/sfx/button${1 + Math.floor(Math.random() * 3)}.mp3`);
+        audio.play();
+    });
+});
+
+document.querySelector(".music-toggle")?.addEventListener("click", togglePlay);
